1c176760fdb84783ec7515807bd9c1fb65fe8b50
JPADN <jpadnalemao@gmail.com>
2023-03-12 13:32:37 -0300

    Updated run.sh and application's JAR

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ zip -d SigningUtilities.jar META-INF/*.RSA META-INF/*.DSA META-INF/*.SF
 cd ../../..
 
 docker build . -t signing-utilities
-docker run -v etapas_1_2_output:/output signing-utilities
+docker run -v ${PWD}/etapas_1_2_output:/output signing-utilities

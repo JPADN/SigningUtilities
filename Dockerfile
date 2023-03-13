98e4aa7bd371240941b6878c29a4f76f61d061a1
JPADN <jpadnalemao@gmail.com>
2023-03-13 12:54:42 -0300

    Update Dockerfile and run script

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
+FROM maven:3.8.5-openjdk-17 AS builder
+WORKDIR /app
+COPY pom.xml ./
+RUN mvn dependency:go-offline
+COPY src src
+RUN mvn clean package
+
 FROM openjdk:17
-COPY out/artifacts/signingutilities_jar/signingutilities.jar app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+WORKDIR /app
+COPY --from=builder /app/target/signingutilities-1.0.jar app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-cd out/artifacts/signingutilities_jar
-zip -d signingutilities.jar META-INF/*.RSA META-INF/*.DSA META-INF/*.SF
-
-cd ../../..
+#cd out/artifacts/signingutilities_jar
+#zip -d signingutilities.jar META-INF/*.RSA META-INF/*.DSA META-INF/*.SF
+#
+#cd ../../..
 
 docker build . -t signing-utilities
-docker run -v ${PWD}/etapas_1_2_output:/output signing-utilities
+docker run -v ${PWD}/etapas_1_2_output:/app/etapas_1_2_output signing-utilities
